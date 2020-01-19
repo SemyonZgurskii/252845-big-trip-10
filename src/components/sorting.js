7d9fb34ec338130f89310124dba4580b29985189
Семен Згурский <saymon221090@gmail.com>
2020-01-19 19:32:15 +0300

    Добавляет возможность сортировать события по цене и продолжительности. Сортировка по умолчанию не доделана

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,17 +1,23 @@
 import AbstractComponent from './abstract-component.js';
 
+export const SortType = {
+  DEFAULT: `default`,
+  TIME: `time`,
+  PRICE: `price`
+};
+
 const createSortingTemplate = () => {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
   <span class="trip-sort__item  trip-sort__item--day">Day</span>
 
   <div class="trip-sort__item  trip-sort__item--event">
     <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
-    <label class="trip-sort__btn" for="sort-event">Event</label>
+    <label data-sort-type="${SortType.DEFAULT}" class="trip-sort__btn" for="sort-event">Event</label>
   </div>
 
   <div class="trip-sort__item  trip-sort__item--time">
     <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-    <label class="trip-sort__btn" for="sort-time">
+    <label data-sort-type="${SortType.TIME}" class="trip-sort__btn" for="sort-time">
       Time
       <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
         <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
@@ -21,7 +27,7 @@ const createSortingTemplate = () => {
 
   <div class="trip-sort__item  trip-sort__item--price">
     <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
-    <label class="trip-sort__btn" for="sort-price">
+    <label data-sort-type="${SortType.PRICE}" class="trip-sort__btn" for="sort-price">
       Price
       <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
         <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
@@ -36,10 +42,30 @@ const createSortingTemplate = () => {
 export default class SortingComponent extends AbstractComponent {
   constructor() {
     super();
-    this._element = null;
+
+    this._currentSortType = SortType.DEFAULT;
   }
 
   getTemplate() {
     return createSortingTemplate();
+  }
+
+  onSortTypeChange(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+
+      if (!evt.target.classList.contains(`trip-sort__btn`)) {
+        return;
+      }
+
+      let sortType = evt.target.dataset.sortType;
+
+      if (this._currentSortType === sortType && sortType !== SortType.DEFAULT) {
+        sortType = sortType + `Reversed`;
+      }
+
+      this._currentSortType = sortType;
+
+      handler(this._currentSortType);
+    });
   }
 }
--- src/controller/trip-controller.js
+++ src/controller/trip-controller.js
@@ -1,12 +1,16 @@
 import EventComponent from '../components/event.js';
 import EditFormComponent from '../components/edit-form.js';
-import SortingComponent from '../components/sorting.js';
+import SortingComponent, {SortType} from '../components/sorting.js';
 import DaysListComponent from '../components/days-list.js';
 import DayComponent from '../components/days.js';
 import NoEventsComponent from '../components/no-events.js';
 
 import {replace, render, RenderPosition} from '../utils/render.js';
 import {transferEventTypes, actionEventTypes, cities} from '../mock/days.js';
+
+const getDurationEnequality = (event) => {
+  return (event.endDate - event.startDate);
+};
 
 const renderEvent = (eventData, container) => {
   const eventComponent = new EventComponent(eventData);
@@ -49,20 +53,57 @@ export default class TripController {
     this._sortingComponent = new SortingComponent();
     this._daysListComponent = new DaysListComponent();
     this._noEventsComponent = new NoEventsComponent();
+    this._days = [];
+    this._daysCopy = [];
   }
 
+
   render(days) {
+
+    this._days = days;
+    this._daysCopy = this._days.slice();
+
+    const renderDays = (daysArray, container) => {
+      daysArray.forEach((it, i) => {
+        const day = new DayComponent(it, i + 1);
+        it.forEach((event) => renderEvent(event, day.getElement()));
+        render(container, day, RenderPosition.BEFOREEND);
+      });
+    };
+
     if (days.length > 1) {
       render(this._container, this._sortingComponent, RenderPosition.AFTERBEGIN);
       render(this._container, this._daysListComponent, RenderPosition.BEFOREEND);
       const eventsListContainerElement = this._container.querySelector(`.trip-days`);
-      days.forEach((it, i) => {
-        const day = new DayComponent(it, i + 1);
-        it.forEach((event) => renderEvent(event, day.getElement()));
-        render(eventsListContainerElement, day, RenderPosition.BEFOREEND);
-      });
+      renderDays(days, eventsListContainerElement);
     } else {
       render(this._container, this._noEventsComponent.getElement(), RenderPosition.BEFOREEND);
     }
+
+    this._sortingComponent.onSortTypeChange((sortType) => {
+      let sortedDays = [];
+      switch (sortType) {
+        case SortType.PRICE:
+          sortedDays = this._daysCopy.slice().map((it) => it.sort((a, b) => a.price - b.price));
+          break;
+        case (SortType.PRICE + `Reversed`):
+          sortedDays = this._daysCopy.slice().map((it) => it.sort((a, b) => b.price - a.price));
+          break;
+        case SortType.TIME:
+          sortedDays = this._daysCopy.slice().map((it) => it.sort((a, b) => getDurationEnequality(a) - getDurationEnequality(b)));
+          break;
+        case (SortType.TIME + `Reversed`):
+          sortedDays = this._daysCopy.slice().map((it) => it.sort((a, b) => getDurationEnequality(b) - getDurationEnequality(a)));
+          break;
+        case SortType.DEFAULT:
+          sortedDays = this._days.slice();
+          this._daysCopy = this._days;
+          break;
+      }
+
+      const eventsListContainerElement = this._container.querySelector(`.trip-days`);
+      eventsListContainerElement.innerHTML = ``;
+      renderDays(sortedDays, eventsListContainerElement);
+    });
   }
 }
